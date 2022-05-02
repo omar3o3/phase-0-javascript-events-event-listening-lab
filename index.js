bf0c50ec32c4c7ae8fb5a95d0f03a619306b76af
Omar <otantawy53@gmail.com>
2022-05-02 12:48:51 -0400

    commit tree 14392287090b1932e9c754a9367d686b93be417a

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
+
+// const input = document.getElementById('input');
+
+// input.addEventListener('click' , function(){
+//     alert("I was clicked!");
+// })
+
+// input.addEventListener('click' , clickAlert)
+
+// function clickAlert(){
+//     alert('I was clicked!');
+// }
+
+
+//********************won't pass********************
+// input.addEventListener('click' , addingEventListener)
+
+// function addingEventListener() {
+//     alert('I was clicked!');
+// }
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+//********************won't pass********************
+
+//********************call back function attempt********************
+// input.addEventListener('click' , alertCallBack)
+
+// const alertCallBack = () => alert('I was clicked!');
+
+// alertCallBack();
+//^^^^^^ adding this line doesnt change anything
+//********************call back function attempt********************
